4e2bf8a37ce302b01dcfa4c409ff58736a4c7d04
Evan Senter <evansenter@gmail.com>
2013-12-30 18:23:06 -0500

    Don't ever extern C headers with C++ code. Whoops copy/paste explosion.

--- h/shared/libfftbor2d_header.h
+++ h/shared/libfftbor2d_header.h
@@ -1,16 +1,8 @@
 #ifndef LIBFFTBOR2D_HEADER_H
 #define LIBFFTBOR2D_HEADER_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include "fftbor2d/params.h"
 #include "fftbor2d/initializers.h"
 #include "fftbor2d/functions.h"
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
