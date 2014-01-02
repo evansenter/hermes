8fef6a818887860bc56cb7bfd3095fb85f1ca45c
Evan Senter <evansenter@gmail.com>
2014-01-01 19:43:27 -0500

    mashup/mfpt_from_energy_grid

--- mashup/mfpt_from_energy_grid/mfpt_from_energy_grid.cpp
+++ mashup/mfpt_from_energy_grid/mfpt_from_energy_grid.cpp
@@ -3,7 +3,6 @@
 #include <string.h>
 #include "shared/libfftbor2d_header.h"
 #include "shared/libmfpt_header.h"
-#include "shared/libspectral_header.h"
 
 KLP_MATRIX convert_fftbor2d_output_to_klp_matrix(const FFTBOR2D_DATA);
 void convert_klp_matrix_to_use_epsilon(KLP_MATRIX*, MFPT_PARAMS*, double);
@@ -13,8 +12,8 @@ int main(int argc, char** argv) {
   FFTBOR2D_DATA fftbor2d_data;
   KLP_MATRIX klp_matrix;
   MFPT_PARAMS mfpt_params;
-  SPECTRAL_PARAMS spectral_params;
   double* transition_matrix;
+  double mfpt;
   
   fftbor2d_params = parse_fftbor2d_args(argc, argv);
   fftbor2d_data   = fftbor2d_from_params(fftbor2d_params);
@@ -25,27 +24,16 @@ int main(int argc, char** argv) {
   mfpt_params.epsilon              = 1e-8;
   mfpt_params.max_dist             = fftbor2d_data.row_length;
   mfpt_params.bp_dist              = fftbor2d_data.bp_dist;
+
+  if (fftbor2d_params.verbose) {
+    debug_mfpt_parameters(mfpt_params);
+  }
   
   klp_matrix        = convert_fftbor2d_output_to_klp_matrix(fftbor2d_data);
   transition_matrix = convert_klp_matrix_to_transition_matrix(&klp_matrix, &mfpt_params);
+  mfpt              = compute_mfpt(&klp_matrix, mfpt_params, transition_matrix);
   
-  spectral_params = init_spectral_params();
-  spectral_params.sequence        = fftbor2d_params.sequence;
-  spectral_params.start_structure = fftbor2d_params.structure_1;
-  spectral_params.end_structure   = fftbor2d_params.structure_2;
-  spectral_params.start_index     = mfpt_params.start_state;
-  spectral_params.end_index       = mfpt_params.end_state;
-  spectral_params.start_time      = -12;
-  spectral_params.end_time        = 12;
-  spectral_params.step_size       = 1;
-  
-  if (fftbor2d_params.verbose) {
-    debug_mfpt_parameters(mfpt_params);
-    debug_spectral_parameters(spectral_params);
-    print_klp_matrix(klp_matrix);
-  }
-  
-  population_proportion_from_row_ordered_transition_matrix(spectral_params, transition_matrix, klp_matrix.length);
+  printf("%f\n", mfpt);
   
   return 0;
 }
@@ -64,3 +52,4 @@ KLP_MATRIX convert_fftbor2d_output_to_klp_matrix(const FFTBOR2D_DATA fftbor2d_da
   
   return klp_matrix;
 }
+
