6b7297cbb87cee9c188b568cdba95b4491787fb1
Evan Senter <evansenter@gmail.com>
2014-03-05 17:50:06 -0500

    The amount of time that took to find was annoying.

--- mashup/population_from_energy_grid/population_from_energy_grid.cpp
+++ mashup/population_from_energy_grid/population_from_energy_grid.cpp
@@ -15,20 +15,21 @@ int main(int argc, char** argv) {
   MFPT_PARAMS mfpt_params;
   SPECTRAL_PARAMS spectral_params;
   double* transition_matrix;
-  
+
   fftbor2d_params = parse_fftbor2d_args(argc, argv);
   fftbor2d_data   = fftbor2d_from_params(fftbor2d_params);
-  
+
   mfpt_params = init_mfpt_params();
+  mfpt_params.rate_matrix          = 1;
   mfpt_params.single_bp_moves_only = 1;
   mfpt_params.hastings             = 1;
   mfpt_params.epsilon              = 1e-8;
   mfpt_params.max_dist             = fftbor2d_data.row_length;
   mfpt_params.bp_dist              = fftbor2d_data.bp_dist;
-  
+
   klp_matrix        = convert_fftbor2d_output_to_klp_matrix(fftbor2d_data);
   transition_matrix = convert_klp_matrix_to_transition_matrix(&klp_matrix, &mfpt_params);
-  
+
   spectral_params = init_spectral_params();
   spectral_params.sequence        = fftbor2d_params.sequence;
   spectral_params.start_structure = fftbor2d_params.structure_1;
@@ -38,29 +39,30 @@ int main(int argc, char** argv) {
   spectral_params.start_time      = -12;
   spectral_params.end_time        = 12;
   spectral_params.step_size       = 1;
-  
+
   if (fftbor2d_params.verbose) {
     debug_mfpt_parameters(mfpt_params);
     debug_spectral_parameters(spectral_params);
     print_klp_matrix(klp_matrix);
+    print_transition_matrix(klp_matrix, transition_matrix);
   }
-  
+
   population_proportion_from_row_ordered_transition_matrix(spectral_params, transition_matrix, klp_matrix.length);
-  
+
   return 0;
 }
 
 KLP_MATRIX convert_fftbor2d_output_to_klp_matrix(const FFTBOR2D_DATA fftbor2d_data) {
   int i;
   KLP_MATRIX klp_matrix;
-  
+
   klp_matrix = init_klp_matrix(fftbor2d_data.non_zero_count);
-  
+
   for (i = 0; i < klp_matrix.length; ++i) {
     klp_matrix.k[i] = fftbor2d_data.non_zero_indices[i] / fftbor2d_data.row_length;
     klp_matrix.l[i] = fftbor2d_data.non_zero_indices[i] % fftbor2d_data.row_length;
     klp_matrix.p[i] = fftbor2d_data.probabilities[fftbor2d_data.non_zero_indices[i]];
   }
-  
+
   return klp_matrix;
 }
