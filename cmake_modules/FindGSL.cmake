d7ad8a8e1317d8d430a14d73a6cba8fccc081343
Evan Senter <evansenter@gmail.com>
2014-06-17 15:24:30 -0400

    Touchups.

--- cmake_modules/FindGSL.cmake
+++ cmake_modules/FindGSL.cmake
@@ -14,6 +14,7 @@ endif (GSL_INCLUDES)
 find_path (GSL_INCLUDES gsl/gsl_math.h)
 
 find_library (GSL_LIB NAMES gsl)
+find_library (GSL_LIB NAMES gslcblas)
 
 set (GSL_CBLAS_LIB "" CACHE FILEPATH "If your program fails to link
 (usually because GSL is not automatically linking a CBLAS and no other
@@ -22,7 +23,7 @@ this variable to a valid CBLAS.  Usually GSL is distributed with
 libgslcblas.{a,so} (next to GSL_LIB) which you may use if an optimized
 CBLAS is unavailable.")
 
-set (GSL_LIBRARIES "${GSL_LIB}" "${GSL_CBLAS_LIB}")
+set (GSL_LIBRARIES ${GSL_LIB} ${GSL_CBLAS_LIB})
 
 # handle the QUIETLY and REQUIRED arguments and set GSL_FOUND to TRUE if
 # all listed variables are TRUE
