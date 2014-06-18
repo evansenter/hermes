0fa11b085702fb509f52ddfb80f1266b9e064813
Evan Senter <evansenter@gmail.com>
2014-06-18 15:27:05 -0400

    EXTERN

--- h/vienna/externs.h
+++ h/vienna/externs.h
@@ -1,11 +1,19 @@
 #ifndef VIENNA_EXTERNS_H
 #define VIENNA_EXTERNS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern double temperature;
 extern int dangles;
 extern int noLonelyPairs;
 extern int subopt_sorted;
 
 #include "functions.h"
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
